fdab718c5b18a8de7acdc28794325206879e3ca1
wert2024 <158856496+wert2024@users.noreply.github.com>
2024-06-20 21:01:02 +0300

    Add files via upload

---
+++ калькулятор.js
@@ -0,0 +1,68 @@
+jQuery('document').ready(function(){
+
+console.log(0);
+	
+	
+	$.ajax('https://github.com/wert2024/Like8', {
+		success: function () {
+			console.log('I get response');
+		}
+	});
+	
+	
+	
+	/*
+ document.querySelector('.btn').onclick = function () {
+ let count = document.getElementById('count');
+ let value = count.innerHTML;
+ ++value;
+ console.log(value);
+ document.getElementById('count').innerHTML = value;
+ }
+*/
+ 
+
+
+console.log(1);	
+
+ 
+});
+
+//------------------------------------------
+ /*     
+console.log(0);
+
+$.ajax('https://jsonplaceholder.typicode.com/posts', {
+	success: function(){
+		console.log('I get response');
+	}
+});
+
+console.log(1);	
+console.log(2);
+console.log(3);	
+console.log(4);
+console.log(5);      
+ */      
+ 
+     
+     
+     
+ 
+     
+     
+
+
+
+  
+     
+
+
+
+
+
+
+
+
+
+
